7b7fefaf59706b7c919eb3d9afe0b37f6d61e49b
pradeep491 <k.pradeep0491@gmail.com>
2024-11-09 21:09:16 +0530

    committed changes

---
+++ springboot-rabbitmq-tutorial/src/main/java/net/javaguides/springboot/publisher/RabbitMQProducer.java
@@ -0,0 +1,28 @@
+package net.javaguides.springboot.publisher;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+public class RabbitMQProducer {
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public RabbitMQProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(String message) {
+        log.info(String.format("Message Sent ->%s", message));
+        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+    }
+}
